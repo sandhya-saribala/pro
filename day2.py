aac63b28067372d3a05136c8b0475ac625c45d2c
sandhya-saribala <saribalasandhya@gmail.com>
2025-03-21 21:36:05 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,19 @@
+def add(a,b):
+    print(a+b)
+add(1,2)
+
+def greet(name):
+    print("hello"+name)
+greet("everyone")
+
+def square(c):
+    print(c*c)
+square(10)
+
+def mul(e,f):
+    print(e*f)
+mul(10,20)
+
+def str_size(word):
+    print(len(word))
+str_size("happy birthday")
